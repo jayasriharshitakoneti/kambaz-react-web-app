58820e0a2fafeedb35789a9ca49cdd5bd0efc034
jayasriharshitakoneti <harshitakoneti@gmail.com>
2025-02-05 21:20:38 -0500

    labs done

---
+++ src/Labs/Lab2/Float.tsx
@@ -0,0 +1,113 @@
+export default function Float() {
+  return (
+    <div id="wd-float-divs">
+      <h2>Float</h2>
+      <div>
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur
+        nesciunt in repellat iste, dolorum autem, voluptatibus quod esse,
+        incidunt quos praesentium similique nulla! Odio quo corporis libero
+        delectus debitis est aperiam rerum ratione a excepturi nesciunt officiis
+        suscipit sequi necessitatibus, explicabo maiores magni repudiandae?
+        Tempore dolore dignissimos quaerat voluptatum ea porro aliquam!
+        Quibusdam doloribus fuga, sequi omnis tempore perspiciatis asperiores.
+        Repudiandae blanditiis saepe consequuntur deserunt officia sed vitae,
+        magnam assumenda, maiores possimus non consectetur vel animi iure
+        voluptate velit nulla ducimus. Maiores repudiandae voluptas perferendis
+        labore veniam dignissimos aspernatur aliquid reiciendis sapiente, quod
+        laborum ipsa praesentium dolor vero expedita tempore earum mollitia
+        aperiam eligendi maxime alias dolorem, laboriosam suscipit possimus? Qui
+        dolor ad architecto voluptates ducimus sed cupiditate voluptate, nobis
+        suscipit recusandae cum, ea unde quibusdam nemo magni, accusamus
+        excepturi eligendi laborum non veniam pariatur quod! Eveniet explicabo
+        maxime temporibus laudantium mollitia quam?
+        <img
+          className="wd-float-left"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur
+        nesciunt in repellat iste, dolorum autem, voluptatibus quod esse,
+        incidunt quos praesentium similique nulla! Odio quo corporis libero
+        delectus debitis est aperiam rerum ratione a excepturi nesciunt officiis
+        suscipit sequi necessitatibus, explicabo maiores magni repudiandae?
+        Tempore dolore dignissimos quaerat voluptatum ea porro aliquam!
+        Quibusdam doloribus fuga, sequi omnis tempore perspiciatis asperiores.
+        Repudiandae blanditiis saepe consequuntur deserunt officia sed vitae,
+        magnam assumenda, maiores possimus non consectetur vel animi iure
+        voluptate velit nulla ducimus. Maiores repudiandae voluptas perferendis
+        labore veniam dignissimos aspernatur aliquid reiciendis sapiente, quod
+        laborum ipsa praesentium dolor vero expedita tempore earum mollitia
+        aperiam eligendi maxime alias dolorem, laboriosam suscipit possimus? Qui
+        dolor ad architecto voluptates ducimus sed cupiditate voluptate, nobis
+        suscipit recusandae cum, ea unde quibusdam nemo magni, accusamus
+        excepturi eligendi laborum non veniam pariatur quod! Eveniet explicabo
+        maxime temporibus laudantium mollitia quam?
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur
+        nesciunt in repellat iste, dolorum autem, voluptatibus quod esse,
+        incidunt quos praesentium similique nulla! Odio quo corporis libero
+        delectus debitis est aperiam rerum ratione a excepturi nesciunt officiis
+        suscipit sequi necessitatibus, explicabo maiores magni repudiandae?
+        Tempore dolore dignissimos quaerat voluptatum ea porro aliquam!
+        Quibusdam doloribus fuga, sequi omnis tempore perspiciatis asperiores.
+        Repudiandae blanditiis saepe consequuntur deserunt officia sed vitae,
+        magnam assumenda, maiores possimus non consectetur vel animi iure
+        voluptate velit nulla ducimus. Maiores repudiandae voluptas perferendis
+        labore veniam dignissimos aspernatur aliquid reiciendis sapiente, quod
+        laborum ipsa praesentium dolor vero expedita tempore earum mollitia
+        aperiam eligendi maxime alias dolorem, laboriosam suscipit possimus? Qui
+        dolor ad architecto voluptates ducimus sed cupiditate voluptate, nobis
+        suscipit recusandae cum, ea unde quibusdam nemo magni, accusamus
+        excepturi eligendi laborum non veniam pariatur quod! Eveniet explicabo
+        maxime temporibus laudantium mollitia quam?
+        <img
+          className="wd-float-left"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur
+        nesciunt in repellat iste, dolorum autem, voluptatibus quod esse,
+        incidunt quos praesentium similique nulla! Odio quo corporis libero
+        delectus debitis est aperiam rerum ratione a excepturi nesciunt officiis
+        suscipit sequi necessitatibus, explicabo maiores magni repudiandae?
+        Tempore dolore dignissimos quaerat voluptatum ea porro aliquam!
+        Quibusdam doloribus fuga, sequi omnis tempore perspiciatis asperiores.
+        Repudiandae blanditiis saepe consequuntur deserunt officia sed vitae,
+        magnam assumenda, maiores possimus non consectetur vel animi iure
+        voluptate velit nulla ducimus. Maiores repudiandae voluptas perferendis
+        labore veniam dignissimos aspernatur aliquid reiciendis sapiente, quod
+        laborum ipsa praesentium dolor vero expedita tempore earum mollitia
+        aperiam eligendi maxime alias dolorem, laboriosam suscipit possimus? Qui
+        dolor ad architecto voluptates ducimus sed cupiditate voluptate, nobis
+        suscipit recusandae cum, ea unde quibusdam nemo magni, accusamus
+        excepturi eligendi laborum non veniam pariatur quod! Eveniet explicabo
+        maxime temporibus laudantium mollitia quam?
+        <div className="wd-float-done"></div>
+      </div>
+      <div id="wd-float-divs">
+        <h2>Float</h2>
+        <div>
+          <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+            Yellow{" "}
+          </div>
+          <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+            Blue{" "}
+          </div>
+          <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+            Red{" "}
+          </div>
+          <img
+            className="wd-float-right"
+            src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+          />
+          <div className="wd-float-done"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
